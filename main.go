0e57217fb1f23c8712e01c70898d710fccbdb01e
badido18 <hm_boudis@esi.dz>
2020-04-15 01:42:31 +0100

    Syntax part #2 + Web server init

--- main.go
+++ main.go
@@ -4,13 +4,64 @@ import (
 	"fmt"
 	"math"
 	"./mypkgfuncs"
+	"net/http"
 )
 
+//normal function
+
 func DisplayHello(n string) string {
-	return "Hello "+ n
+	return "Hello " + n
 }
 
-func main(){
+//closures
+
+func Closure() func(string) int{
+	Nmes := ""
+	return func(a string) int{
+		Nmes += a
+		return len(Nmes)
+	} 
+}
+
+//a struct 
+
+type Bike struct {
+	brand string
+	typ string
+	age int
+}
+
+//methods
+
+func (bike Bike) DisplayAge() {
+		fmt.Printf("its age : %d months\n",bike.age)
+}
+
+func (bike *Bike) DecremAge(){
+	bike.age--
+}
+
+//interface
+
+type Caracteristics interface {
+	speed() int
+}
+
+func (bike Bike) speed() int {
+	fmt.Println("the speed is not defined hahaah", bike.brand)
+	return 0
+}
+
+//mywebserver function
+
+func index (writer http.ResponseWriter , Request *http.Request){
+	fmt.Fprintf(writer, "<H1>My Go Server <H1>")
+}
+
+
+//main function
+
+func main() {
 
 	//data types
 	a := "this is a string"
@@ -20,20 +71,20 @@ func main(){
 	const e float64 = 6e48
 
 	//IO functions
-	fmt.Println("Let's GO !",a,b,c,d+e)
-	fmt.Printf("%T\n",e)
-	fmt.Println(math.Max(math.Round(0.7),math.Floor(2.7)))
+	fmt.Println("Let's GO !", a, b, c, d+e)
+	fmt.Printf("%T\n", e)
+	fmt.Println(math.Max(math.Round(0.7), math.Floor(2.7)))
 	fmt.Println(mypkgfuncs.Getpi())
 	fmt.Print("Enter your name : ")
 
 	//taking input
 	var name string
-	fmt.Scanf("%s",&name)
+	fmt.Scanf("%s", &name)
 	fmt.Println(DisplayHello(name))
 
 	//Arrays
-	var Names =  [5]string{"Moh","Bernard","Asperge","Bob","Banana"}
-	Ages := []int{20,21,23,24,25}
+	var Names = [5]string{"Moh", "Bernard", "Asperge", "Bob", "Banana"}
+	Ages := []int{20, 21, 23, 24, 25}
 	fmt.Println(Names[1:3])
 	fmt.Println(Ages[2:5])
 
@@ -44,21 +95,24 @@ func main(){
 		fmt.Println("he is not in :( ")
 	}
 	switch name {
-	case "madjid" : fmt.Println("Yeah",name) 
-	case "moh" : fmt.Println("Yeah",name)
-	default : fmt.Println("not known")
+	case "madjid":
+		fmt.Println("Yeah", name)
+	case "moh":
+		fmt.Println("Yeah", name)
+	default:
+		fmt.Println("not known")
 	}
 
 	//Loops
-	for i := 0 ; i < len(Names) ; i++ {
-		fmt.Printf("%s \n",DisplayHello(Names[i]))
+	for i := 0; i < len(Names); i++ {
+		fmt.Printf("%s \n", DisplayHello(Names[i]))
 	}
-	
+
 	//maps
 
 	Persons := make(map[string]int)
 	Persons[name] = 20
-	for i := 0 ; i < len(Names) ; i++ {
+	for i := 0; i < len(Names); i++ {
 		Persons[Names[i]] = Ages[i]
 	}
 	fmt.Println(Persons)
@@ -67,23 +121,63 @@ func main(){
 	tmp[1] = Ages
 	fmt.Println(tmp)
 
-	delete(Persons,"Asperge")
+	delete(Persons, "Asperge")
 	fmt.Println(Persons)
 
-	infos := map[string]int{"A" : 0x41}
+	infos := map[string]int{"A": 0x41}
 	fmt.Println(infos)
 
-	//ranges 
-	
+	//ranges
+
 	//for looping through maps
 
-	for name, age  := range Persons {
-		fmt.Printf("%s is %d \n",name,age)
+	for name, age := range Persons {
+		fmt.Printf("%s is %d \n", name, age)
 	}
-	//either this way in an array 
-	for  _,x := range Ages {
+	//either this way in an array
+	for _, x := range Ages {
 		fmt.Println(x)
 	}
+
+	// pointers like C very useful
+	num := 10
+	address := &num //a pointer
+	fmt.Println(num,address)
+	fmt.Println(*address , *&num)
+
+	//Closure 
+	//Look at the top to find the closure 
+	na := Closure()
+	fmt.Println(na("Madjid"))
+
+	//Structs testing
+	//look at the top to find it
+	Mybike := Bike{brand : "GOsport" , typ : "VTT" , age : 1}
+
+	fmt.Println(Mybike)
+
+	Mybike = Bike{ "ClimMountain","offroad",2}
+	Mybike.age++
+
+	//using methods defined on top
+
+	Mybike.DisplayAge()
+	Mybike.DecremAge()
+	Mybike.DisplayAge()
+
+	//using interfaces
+	//this is a test an has no meaning for using interfaces
+	Mybike.speed()
+
+	//web test
+	//running a web server
+
+
+	PORT := "3000"
+	http.HandleFunc("/",index)
+	fmt.Println("Starting at Port :",PORT)
+	http.ListenAndServe(":"+PORT,nil)
+
 
 
 }
